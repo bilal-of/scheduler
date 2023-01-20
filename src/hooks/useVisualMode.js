a36ba4ea580d4ecd3276cb1c39d863aa9779e47d
Bilal Farah <bfarah@ualberta.ca>
2023-01-19 23:00:39 -0700

    debugging and cleaning up code

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,28 +1,28 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial); 
+  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   const transition = (newMode, replace) => {
     setMode(newMode)
     setHistory(prev => {
-      if(replace){
+      if (replace) {
         let newHistory = [...prev]
         newHistory.pop()
         newHistory.push(newMode)
         return newHistory
-        } else {
-            return [...prev,newMode]
-        }
+      } else {
+        return [...prev, newMode]
+      }
     })
   }
-  function back() {   
-    if (history.length > 1) { 
-      const newHistory = [...history] 
-      newHistory.pop() 
-      setHistory(newHistory) 
-      setMode(newHistory[newHistory.length - 1]) 
+  function back() {
+    if (history.length > 1) {
+      const newHistory = [...history]
+      newHistory.pop()
+      setHistory(newHistory)
+      setMode(newHistory[newHistory.length - 1])
     }
   }
   return { mode, history, transition, back };
